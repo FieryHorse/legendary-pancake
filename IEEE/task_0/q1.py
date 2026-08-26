ddc6d1c3e16d9d18efe9be898c995d635f362264
Aryan Saha <aryansaha08@gmail.com>
2026-08-26 15:20:01 +0530

    Save local changes before pulling

--- IEEE/task_0/q1.py
+++ IEEE/task_0/q1.py
@@ -1,27 +1,27 @@
 N = int(input())
-l = list(map(int, input().split()))
-greatest = l[0]
+my_list = list(map(int, input().split()))
+greatest = my_list[0]
 for i in range(1, N):
-    if l[i] > greatest:
-        greatest = l[i]
+    if my_list[i] > greatest:
+        greatest = my_list[i]
 print("Largest:", greatest)
-least = l[0]
+least = my_list[0]
 for i in range(1, N):
-    if l[i] < least:
-        least = l[i]
+    if my_list[i] < least:
+        least = my_list[i]
 print("Smallest:", least)
 sum = 0
 for i in range(N):
-    sum += l[i]
+    sum += my_list[i]
 print("Sum:", sum)
 even_count = 0
 odd_count = 0
 for i in range(N):
-    if l[i] % 2 == 0:
+    if my_list[i] % 2 == 0:
         even_count += 1
     else:
         odd_count += 1
 print("Even count:", even_count)
 print("Odd count:", odd_count)
-print("Reversed:", l[::-1])
+print("Reversed:", *(my_list[::-1]))
 
